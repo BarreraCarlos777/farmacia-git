2b31ecac65e42840d971bea59cdb10b490a268fb
Ian Salvador <2024310397@uteq.edu.mx>
2025-08-07 11:50:18 -0600

    Agregar clase Paciente

--- pacientes.py
+++ pacientes.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # pacientes.py
 
 class Paciente:
@@ -22,3 +23,17 @@ if __name__ == "__main__":
         print("Es mayor de edad")
     else:
         print("Es menor de edad")
+=======
+class Doctor:
+    def __init__(self, medico_id, nombre, especialidad):
+        self.medico_id=medico_id
+        self.nombre=nombre
+        self.especialidad=especialidad
+
+    def presentar(self):
+        return f"Doctor {self.nombre} (ID: {self.medico_id}), especialidad: {self.especialidad}"
+    
+if __name__ == "__main__":
+    doc=Doctor(101,"García", "Cardiología")
+    print(doc.presentar())
+>>>>>>> Stashed changes
