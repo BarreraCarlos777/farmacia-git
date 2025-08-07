20486799b6b0ca27352ccbb74daacbc18de75685
I4nSudo <2024310397@uteq.edu.mx>
2025-08-07 11:53:29 -0600

    Add files via upload

---
+++ pacientes.py
@@ -0,0 +1,39 @@
+<<<<<<< Updated upstream
+# pacientes.py
+
+class Paciente:
+    def __init__(self, nombre, edad, genero):
+        self.nombre = nombre
+        self.edad = edad
+        self.genero = genero
+
+    def mostrar_info(self):
+        return f"Nombre: {self.nombre}, Edad: {self.edad}, Género: {self.genero}"
+
+    def es_mayor_de_edad(self):
+        return self.edad >= 18
+
+
+
+if __name__ == "__main__":
+    paciente1 = Paciente("Juan Pérez", 30, "Masculino")
+    print(paciente1.mostrar_info())
+
+    if paciente1.es_mayor_de_edad():
+        print("Es mayor de edad")
+    else:
+        print("Es menor de edad")
+=======
+class Doctor:
+    def __init__(self, medico_id, nombre, especialidad):
+        self.medico_id=medico_id
+        self.nombre=nombre
+        self.especialidad=especialidad
+
+    def presentar(self):
+        return f"Doctor {self.nombre} (ID: {self.medico_id}), especialidad: {self.especialidad}"
+    
+if __name__ == "__main__":
+    doc=Doctor(101,"García", "Cardiología")
+    print(doc.presentar())
+>>>>>>> Stashed changes
